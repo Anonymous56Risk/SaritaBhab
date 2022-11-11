da2fe840276162240446f465d20b901855b72aac
Anonymous56Risk <110618945+Anonymous56Risk@users.noreply.github.com>
2022-11-11 20:29:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
+        self.API_ID: str = os.environ.get("API_ID", "29399732")
+        self.API_HASH: str = os.environ.get("API_HASH", "11aea6465aa3ddf4bb831a5cc0b825f5")
+        self.SESSION: str = os.environ.get("SESSION", "AQC6fEM8nPEA1GKmo_NnbxqwIA032g3W9uJGuS8zI9IqZCNLfzrx9XDPwDqbGsnlnNsOLIiiYMXZ3b2E8MQNIstN2puexNm8LeQSP64sf-w9vgZfQsia--2qv4iK9HsD9GX7mMe3T5y4BmNr3gST9gaa0j08P0pw8iANerlfLBOhzEyuCfIBk9Nw0OFc2OWjZ_I9u5JjLB-B3GxSS1QYqe2huZIAVXOnnJtZ8Ta7dmS8TUzIWlyQM-7LUH1VjVRigcAO5hU_Cj-XNRgvziRZDJwyfyyjErDyWe24BiqPkrw7TgIIe9GZLPht5C7yGAP4xiCUHRfaqk1SmqWcJFeHGs0YAAAAAVXgRhQA")
         self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
         self.SUDOERS: list = [
             int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
